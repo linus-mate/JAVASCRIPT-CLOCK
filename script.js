4e40c45aea032fa968fc63a81303d40e81fed94a
linus mutugi mate <mlynux@gmail.com>
2022-08-30 20:26:02 +0300

    setting up local timezone clock

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+const month = document.getElementById('month')
+const date = document.getElementById('date');
+const amPm = document.getElementById('am-pm');
+
+
+
+function initializeClock(){
+
+    const now = new Date();
+
+   // const currentMonth = now.getMonth();
+   // console.log(currentMonth); 
+   // month.textContent = currentMonth;
+
+    const currentDate = now.getDate();
+    const hours = (now.getHours() + now.getMinutes() / 60) / 12 * 360;
+    const minutes = (now.getMinutes() + now.getSeconds() / 60) / 60 * 360;
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360);
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutes}deg)`;
+    hourHand.style.transform = `rotate(${hours}deg)`;
+
+    
+    date.textContent = currentDate;
+
+    requestAnimationFrame(initializeClock)
+}
+
+//animationFrame more effective than timeOut
+
+requestAnimationFrame(initializeClock)
